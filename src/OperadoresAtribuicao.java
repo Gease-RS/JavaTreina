556192d8a9a266f3c2773f323b4257bef2d133a7
Gease Oliveira da Rosa <geasebrasil@gmail.com>
2017-07-30 02:49:55 -0300

    Treinamento de Java sincronização arquivos.

---
+++ src/OperadoresAtribuicao.java
@@ -0,0 +1,9 @@
+public class OperadoresAtribuicao {
+    public static void main(String[] args) {
+        // =, -=. +=, *=, /=, %=
+        int salario = 1800;
+        //salario = salario + 1000;
+        salario += 1000;
+        System.out.println(salario);
+    }
+}
